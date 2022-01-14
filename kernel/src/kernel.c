9ba5df36021edaa229b6e8ac6ff3589c08497100
Carlos Damázio <carlos.damazio@damazio.dev>
2022-01-13 21:59:06 -0300

    feat: adding some types to C w/o STDLIB

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,24 +1,8 @@
-typedef unsigned long long size_t;
+#include "output.h"
 
-typedef struct {
-    void* base_address;
-    size_t buffer_sz;
-    unsigned int width;
-    unsigned int height;
-    unsigned int pixels_per_scan_line;
-} Framebuffer;
-
-void kprint(int colour, const char *string)
-{
-    volatile char *vga_buff = (volatile char*) 0xB8000;
-
-    while (*string != 0) { 
-        *vga_buff++ = *string++;
-        *vga_buff++ = colour;
-    }
-}
 
 unsigned int _start(Framebuffer *buff)
 {
+    plot_pixel(buff, 32, 32, 255);
     return buff->width;
 }
