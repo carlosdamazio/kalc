1f728da376ba44992b11e69ac10108b4d3038078
Carlos Damázio <carlos.damazio@damazio.dev>
2022-01-16 10:19:34 -0300

    feat: load font file to kernel, but still getting null font

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,6 +1,6 @@
 #include "output.h"
 
-unsigned int _start(FrameBuffer *fbuff)
+unsigned int _start(FrameBuffer *fbuff, PSF_Font *font)
 {
     draw_line(fbuff, 50, 4, 0xff00ffff);
     return fbuff->PixelsPerScanLine;
