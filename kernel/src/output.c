aa7b2850b77a31aca35c7bf03ff2077e91d7c28d
Carlos Damázio <carlos.damazio@damazio.dev>
2022-01-16 12:30:54 -0300

    feat: preparing print functions

--- kernel/src/output.c
+++ kernel/src/output.c
@@ -6,3 +6,12 @@ draw_line(FrameBuffer *buff, int y, int bbp, unsigned long long pixel)
     for (unsigned int x = 0; x < buff->HorizontalRes / 2 * bbp; x += 4)
         *(unsigned int*)(x + (y * buff->PixelsPerScanLine * bbp) + buff->FrameBufferBase) = pixel;
 }
+
+void
+kput_char(FrameBuffer *buff, PSF1_Font, unsigned int colour, char chr, unsigned int x_off)
+
+void
+kprint(FrameBuffer *buff, PSF1_Font *font, unsigned int colour, char *string)
+{
+    
+}
