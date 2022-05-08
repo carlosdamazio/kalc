f40bf4a10055b12128a8e6364b48b0a2148d86cd
Carlos Damázio <carlos.damazio@damazio.dev>
2022-05-07 22:11:35 -0300

    fix: formatting again

--- boot/src/file.c
+++ boot/src/file.c
@@ -47,7 +47,7 @@ AllocateSegments(EFI_FILE *file, Elf64_Phdr *phdrs, Elf64_Ehdr *header)
 			int pages = (phdr->p_memsz + 0x1000 - 1) / 0x1000;
 			Elf64_Addr segment = phdr->p_paddr;
 			uefi_call_wrapper(BS->AllocatePages, 4, AllocateAddress,
-					EfiLoaderData, pages, &segment);
+				EfiLoaderData, pages, &segment);
 
 			uefi_call_wrapper(file->SetPosition, 2, file,
 				phdr->p_offset);
@@ -69,7 +69,7 @@ LoadFile(EFI_FILE *Directory, CHAR16 *Path, EFI_HANDLE Image)
 		EFI_GUID EfiImageProtocolGuid    = EFI_LOADED_IMAGE_PROTOCOL_GUID;
 		EFI_GUID EfiSimpleFsProtocolGuid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
 		uefi_call_wrapper(BS->HandleProtocol, 3, Image, &EfiImageProtocolGuid,
-				(void **)&loaded_image);
+			(void **)&loaded_image);
 		uefi_call_wrapper(BS->HandleProtocol, 3, loaded_image->DeviceHandle,
 			&EfiSimpleFsProtocolGuid, (void **)&fs_protocol);
 	}
