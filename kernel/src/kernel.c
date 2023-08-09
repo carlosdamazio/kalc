45d60b2ddd9b65f666370406b83d54e02ce96356
Carlos Dmz <hello@carlosdmz.io>
2023-08-09 11:40:04 -0300

    feat: preparing print functions

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,7 +1,7 @@
 #include "output.h"
 
-unsigned int _start(FrameBuffer *fbuff, PSF_Font *font)
+unsigned int _start(FrameBuffer *fbuff, PSF1_Font *font)
 {
     draw_line(fbuff, 50, 4, 0xff00ffff);
-    return fbuff->PixelsPerScanLine;
+    return font->psf_header->charsize;
 }
