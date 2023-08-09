501888e62baedb0e2446680324f56bebf24f5924
Carlos Dmz <hello@carlosdmz.io>
2023-08-09 11:38:54 -0300

    chore: adding some support to ELF compilation

--- boot/main.c
+++ boot/main.c
@@ -6,6 +6,7 @@ EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
    InitializeLib(ImageHandle, SystemTable);
-   Print(L"Hello, world!\n");
+   Print(L"[INFO] Hello, KalcOS\n");
    return EFI_SUCCESS;
 }
+
