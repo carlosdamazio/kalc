42cb61358352834a245cc60cbec280cfea2e1c2c
Carlos Dmz <hello@carlosdmz.io>
2023-08-09 11:38:56 -0300

    fix: don't use VGA, use GOP instead

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,10 +1,14 @@
-#include "output.h"
+void kprint(int colour, const char *string)
+{
+    volatile char *vga_buff = (volatile char*) 0xB8000;
+
+    while (*string != 0) { 
+        *vga_buff++ = *string++;
+        *vga_buff++ = colour;
+    }
+}
 
 void _start()
 {
-    kprint("Hello, world!\n");
-
-    for (;;) {
-
-    }
+    kprint(0xF, "Hello, world!");
 }
